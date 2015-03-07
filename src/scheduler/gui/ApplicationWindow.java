7d9d3790520c19d510b811dc9919e86797e3bcea
skarahoda <skarahoda@sabanciuniv.edu>
2015-03-07 14:44:38 +0200

    Empty window application added to repo

---
+++ src/scheduler/gui/ApplicationWindow.java
@@ -0,0 +1,46 @@
+package scheduler.gui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+import scheduler.data.BannerParser;
+
+public class ApplicationWindow {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ApplicationWindow window = new ApplicationWindow();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public ApplicationWindow() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		BannerParser.getCourses();
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
